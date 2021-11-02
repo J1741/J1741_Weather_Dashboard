3a2f13f1e8307b1d0b65a1fa3bd0f1d63d507815
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-01 21:06:11 -0400

    adding proof-of-concept API call with hard-coded URL

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,36 @@
-// TODO: add function to search open weather API for city name
+// get access to search button
+const searchButton = document.getElementById('search-button');
+const cityInput = document.getElementById('city-input');
+
+// test function for event listeners
+// function sayHello(event) {
+//   event.preventDefault();
+//   console.log("Hello There!");
+// }
+
+// **TESTING** hard-coded API call
+function getOpenWeatherData (event) {
+
+  // prevent page from reloading on click
+  event.preventDefault();
+
+  // **hard-coded** request url
+  // let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Louisville&appid=a903e090203cb951093bb90d5f213895'
+
+  let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Louisville&appid=a903e090203cb951093bb90d5f213895'
+
+  // get current weather for city
+  fetch(requestUrl)
+    .then(function (response) {
+      // console.log("** response is: **\n", response);
+      // console.log("** json is: **\n", response.json());
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("** data is: **\n", data);
+      console.log("data.name")
+    });
+  }
+
+// call getOpenWeatherData when search button clicked
+searchButton.addEventListener('click', getOpenWeatherData);
