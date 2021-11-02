cad15cb8839e98b04a7da441ed28c6a11d68ace0
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-02 12:47:49 -0400

    adding variables to store forecast temperatures

--- assets/script.js
+++ assets/script.js
@@ -27,6 +27,7 @@ function getOpenWeatherData (requestedCity) {
       console.log("** current weather data is: **\n", data);
 
       // get current city
+
       let currentCity = data.name;
       console.log("currentCity is", currentCity);
 
@@ -55,10 +56,12 @@ function getOpenWeatherData (requestedCity) {
       // get current humidity
       let currentHumidity = data.main.humidity
       console.log("currentHumidity is:", currentHumidity);
+      console.log("----")        
 
       // get latitude and longitude from api call
       let cityLat = data.coord.lat;
       let cityLon = data.coord.lon;
+
 
       // get uv index for city
       let uviRequestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&units=imperial&appid=${apiKey}&cnt=1`
@@ -71,8 +74,10 @@ function getOpenWeatherData (requestedCity) {
           console.log("** current uvi data is: **\n", data);
           let currentUvi = data.current.uvi
           console.log("currentUvi is:", currentUvi);
+          console.log("----")        
         })
       
+
       // get 5-day forecast for city
       let fiveDayRequestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${requestedCity}&units=imperial&appid=${apiKey}`
 
@@ -83,12 +88,36 @@ function getOpenWeatherData (requestedCity) {
         .then(function (data) {
           console.log("** 5-day forecast data is: **\n", data)
 
-        // get day 1 weather
-        // get day 2 weather
-        // get day 3 weather
-        // get day 4 weather
-        // get day 5 weather
+        // Need: date, temp, wind, and humidity
 
+        // get day 1 weather
+        console.log("** day 1 data is: **", data.list[0]);
+        // let day1Date = new Date(
+        //   data.dt * 1000
+        // );
+
+        let day1Temp = Math.round(data.list[0].main.temp); 
+        console.log("day1Temp is: ", day1Temp);
+        console.log("----")      
+
+        // get day 2 weather
+        let day2Temp = Math.round(data.list[1].main.temp); 
+        console.log("day2Temp is: ", day2Temp);
+        console.log("----")        
+
+        // get day 3 weather
+        let day3Temp = Math.round(data.list[2].main.temp); 
+        console.log("day3Temp is: ", day3Temp);
+        console.log("----")        
+
+        // get day 4 weather
+        let day4Temp = Math.round(data.list[3].main.temp); 
+        console.log("day4Temp is: ", day4Temp);
+        console.log("----")        
+
+        // get day 5 weather
+        let day5Temp = Math.round(data.list[4].main.temp); 
+        console.log("day5Temp is: ", day5Temp);
         })
       
     })
