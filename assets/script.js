18a0fd06c3812cc9410202e0074ab4813c24e7fb
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-01 21:44:23 -0400

    adding variables for elements of current date

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,12 @@
 // get access to search button
 const searchButton = document.getElementById('search-button');
 const cityInput = document.getElementById('city-input');
-
-// test function for event listeners
-// function sayHello(event) {
-//   event.preventDefault();
-//   console.log("Hello There!");
-// }
+const apiKey = 'a903e090203cb951093bb90d5f213895';
 
 // **TESTING** hard-coded API call
 function getOpenWeatherData (requestedCity) {
 
   let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + requestedCity + '&appid=a903e090203cb951093bb90d5f213895'
-
-  // prevent page from reloading on click
-  // event.preventDefault();
-
-  // **hard-coded** request url
-  // let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Louisville&appid=a903e090203cb951093bb90d5f213895'
-
 
   // get current weather for city
   fetch(requestUrl)
@@ -30,7 +18,40 @@ function getOpenWeatherData (requestedCity) {
     .then(function (data) {
       console.log("** data is: **\n", data);
       console.log("data.name")
-    });
+
+      // format date from api call
+      currentDate = new Date(
+        data.dt * 1000
+      );
+
+      // get current year, month, and date
+      currentYear = currentDate.getFullYear();
+      console.log("currentYear:", currentYear);
+
+      // get current year, month, and date
+      currentMonth = currentDate.getMonth() + 1;
+      console.log("currentMonth:", currentMonth);
+
+      // get current year, month, and date
+      currentDay = currentDate.getDate();
+      console.log("currentDay", currentDay);
+
+      // get lat and long from api call
+      cityLat = data.coord.lat;
+      cityLon = data.coord.lon;
+
+      // create request url to get uv index
+      uviRequestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&appid=${apiKey}&cnt=1`
+
+      // get uv index
+      fetch(uviRequestUrl)
+        .then(function (response) {
+          return response.json()
+        })
+        .then(function (data) {
+          console.log("** data is: **\n", data);
+        })
+    })
   }
 
 // call getOpenWeatherData when search button clicked
